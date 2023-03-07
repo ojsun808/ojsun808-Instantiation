ca0701ef4c8b9ceeb66c8af3e740c7d9b0a3f725
Orion Sun <ojsun808@gmail.com>
2023-03-07 23:20:48 +0000

    Implemented object instantiation

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String out = new String();
+        return out;
     }
 }
